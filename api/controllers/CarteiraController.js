a7b2eee3803193605d6c629030677b3f575e7f31
Alex Castelo <alexcastelo437@gmail.com>
2022-05-13 15:36:37 -0300

    feat: created methods post, put and delete

--- api/controllers/CarteiraController.js
+++ api/controllers/CarteiraController.js
@@ -24,6 +24,45 @@ class CarteiraController { //declara os metodos que serao usados
             return res.status(500).json(error.message)
         }
     }
+
+    static async criaCarteira(req, res) {
+        
+        const novaCarteira = req.body
+        try {
+            const novaCarteiraCriada = await database.Carteira.create(novaCarteira)
+            return res.status(200).json(novaCarteiraCriada)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async atualizaCarteira(req, res) {
+        const { id } = req.params
+        const novaInfos = req.body
+
+         try {
+            await database.Carteira.update(novaInfos, { where: {id: Number(id)}}
+            )
+            const carteiraAtualizada = await database.Carteira.findOne( {where: {
+                id: Number(id)
+            }}) 
+            return res.status(200).json(carteiraAtualizada)
+         } catch (error) {
+            return res.status(500).json(error.message)
+         }
+
+    }
+
+    static async apagaCarteira(req, res) {
+        const { id } = req.params
+        try {
+            await database.Carteira.destroy( { where: { id: Number(id)}})
+           return res.status(200).json({ mensagem: `id ${id} deletado`}) 
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+
+    }
  
 }
 
--- api/routes/carteiraRouter.js
+++ api/routes/carteiraRouter.js
@@ -4,7 +4,12 @@ const CarteiraController = require('../controllers/CarteiraController')
 const router = Router()
 
 router.get('/api/v1/wallet', CarteiraController.listaCarteiras)
-
 router.get('/api/v1/wallet/:id', CarteiraController.pegaUmaCarteira)
+router.post('/api/v1/wallet', CarteiraController.criaCarteira)
+router.put('/api/v1/wallet/:id', CarteiraController.atualizaCarteira)
+router.delete('/api/v1/wallet/:id', CarteiraController.apagaCarteira)
+
 
 module.exports = router
+
+
